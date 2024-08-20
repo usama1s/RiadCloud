69fa48cf509235996f29a2a7927179d3f7c4ccb1
usmanahmed <nousmanahmed19@gmail.com>
2024-08-21 01:37:14 +0500

    modified

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,12 +3,10 @@ const navMenu = document.querySelector(".nav-menu");
 const closeMenuBtn = document.querySelector(".mobile-menu-close");
 gsap.registerPlugin(ScrollTrigger);
 
-// console.log(menuToggleBtn);
-// if (menuToggleBtn && navMenu) {
+
 menuToggleBtn.addEventListener("click", () => {
   navMenu.classList.toggle("active");
   document.body.style.overflow = "hidden";
-  //   document.body.style.overflow = "auto";
   closeMenuBtn.style.display = "block";
 });
 
@@ -20,279 +18,264 @@ closeMenuBtn.addEventListener("click", () => {
   closeMenuBtn.style.display = "none";
 });
 
-    // Animate the background transitions
-    document.querySelectorAll('.parallax-bg').forEach((bg, index) => {
-      gsap.to(bg, {
-        opacity: 1,
-        scrollTrigger: {
-          trigger: bg.parentNode, // Trigger on the parent section
-          start: "20% 100%", // When the top of the section hits the top of the viewport
-          end: "80% 0%", // When the bottom of the section hits the top of the viewport
-          scrub: true,
-          markers: true,
-          // onEnter: () => gsap.to(bg, { opacity: 1 }), // Fade in when entering
-          // onLeave: () => gsap.to(bg, { opacity: 0 }), // Fade out when leaving
-          // onEnterBack: () => gsap.to(bg, { opacity: 1 }), // Fade in when scrolling back
-          // onLeaveBack: () => gsap.to(bg, { opacity: 0 }), // Fade out when scrolling back out
-        }
-      });
-    });
-    
-    // Animate the content movement and image fade in/out
-    document.querySelectorAll('.parallax-content').forEach((content) => {
-      gsap.fromTo(content, 
-        { opacity: 0, y: 50 }, // Start slightly below and invisible
-        { 
-          opacity: 1, 
-          y: 0, 
-          scrollTrigger: {
-            trigger: content.parentNode, // Trigger on the parent section
-            start: "top center",
-            end: "bottom center",
-            scrub: true,
-            toggleActions: "play none none reverse",
-          }
-        }
-      );
-    
-      gsap.fromTo(content.querySelector('img'), 
-        { opacity: 0, y: 50 }, 
-        { 
-          opacity: 1, 
-          y: 0, 
-          scrollTrigger: {
-            trigger: content.parentNode,
-            start: "top center",
-            end: "bottom center",
-            scrub: true,
-            toggleActions: "play none none reverse",
-          }
-        }
-      );
-    });
-    
+// Animate the background transitions
+document.querySelectorAll(".parallax-bg").forEach((bg, index) => {
+  gsap.to(bg, {
+    opacity: 1,
+    scrollTrigger: {
+      trigger: bg.parentNode, // Trigger on the parent section
+      start: "20% 100%", // When the top of the section hits the top of the viewport
+      end: "80% 0%", // When the bottom of the section hits the top of the viewport
+      scrub: true,
+    },
+  });
+});
+
+// Animate the content movement and image fade in/out
+document.querySelectorAll(".parallax-content").forEach((content) => {
+  gsap.fromTo(
+    content,
+    { opacity: 0, y: 50 }, // Start slightly below and invisible
+    {
+      opacity: 1,
+      y: 0,
+      scrollTrigger: {
+        trigger: content.parentNode, // Trigger on the parent section
+        start: "0% 100%",
+        end: "40% center",
+        scrub: true,
+        toggleActions: "play none none reverse",
+      },
+    }
+  );
+
+  gsap.fromTo(
+    content.querySelector("img"),
+    { opacity: 0, y: 50 },
+    {
+      opacity: 1,
+      y: 0,
+      scrollTrigger: {
+        trigger: content.parentNode,
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+        toggleActions: "play none none reverse",
+      },
+    }
+  );
+});
 
 // -------------Effect---------------//
 
-gsap.from("#img32", {
-  y: 400,
+gsap.from("#flower-img", {
+  y: 700,
   scrollTrigger: {
-    trigger: "#img1",
-    start: "0 95%",
-    end: "50% 55%",
+    trigger: "#sec-1",
+    start: "0 100%",
+    end: "95% 50%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
-gsap.from("#text1", {
+gsap.from("#sec-2-text", {
   y: 400,
   scrollTrigger: {
     trigger: "#sec-1",
-    start: "0 95%",
-    end: "50% 85%",
+    start: "0 100%",
+    end: "90% 50%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
 // -------------Effect-2---------------//
 
-
-
-gsap.from("#light", {
-  y: 400,
+gsap.from("#mind-img", {
+  y: 700,
   scrollTrigger: {
-    trigger: "#imglight",
-    start: "0 95%",
-    end: "50% 85%",
+    trigger: "#sec-2",
+    start: "20% 60%",
+    end: "90% 90%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
-
-gsap.from("#imglight1", {
-  y: 400,
-  scrollTrigger: {
-    trigger: "#light1",
-    start: "0 95%",
-    end: "50% 55%",
-    scrub: true,
-  },
-});
-
-gsap.from("#text2", {
+gsap.from("#bootrobo-img", {
   y: 400,
   scrollTrigger: {
     trigger: "#sec-2",
-    start: "0 95%",
-    end: "50% 55%",
+    start: "0% 90%",
+    end: "90% 30%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
+gsap.from("#sec-2-text", {
+  y: 500,
+  scrollTrigger: {
+    trigger: "#sec-2",
+    start: "0 75%",
+    end: "90% 50%",
+    scrub: true,
+    toggleActions: "play none none reverse",
+  },
+});
 
 // -------------Effect-3---------------//
 
-
-
-gsap.from("#key2", {
-  y: 400,
+gsap.from("#key-img", {
+  y: 900,
   scrollTrigger: {
-    trigger: "#key1",
-    start: "0 95%",
-    end: "50% 55%",
+    trigger: "#sec-3",
+    start: "20% 60%",
+    end: "90% 90%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
-
-gsap.from("#bubble2", {
+gsap.from("#bubble1-img", {
   y: 400,
   scrollTrigger: {
-    trigger: "#bubble1",
-    start: "0 95%",
-    end: "50% 55%",
+    trigger: "#sec-3",
+    start: "70 95%",
+    end: "90% 10%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
-gsap.from("#bubble23", {
-  y: 400,
+gsap.from("#bubble2-img", {
+  y: 600,
   scrollTrigger: {
-    trigger: "#bubble21",
+    trigger: "#sec-3",
     start: "0 95%",
-    end: "50% 85%",
+    end: "90% 45%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
-gsap.from("#bubble24", {
-  y: 400,
+gsap.from("#bubble3-img", {
+  y: 500,
   scrollTrigger: {
-    trigger: "#bubble32",
-    start: "0 95%",
-    end: "50% 55%",
+    trigger: "#sec-3",
+    start: "0 50%",
+    end: "90% 90%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
 gsap.from("#text3", {
-  y: 400,
+  y: 600,
   scrollTrigger: {
     trigger: "#sec-3",
-    start: "0 95%",
-    end: "50% 55%",
+    start: "0 75%",
+    end: "90% 50%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
 // -------------Effect-4---------------//
 
-
-
-gsap.from("#brain2", {
-  y: 400,
-  scrollTrigger: {
-    trigger: "#brain1",
-    start: "0 95%",
-    end: "50% 85%",
-    scrub: true,
-  },
-});
-
-
-gsap.from("#hand2", {
-  y: 400,
-  scrollTrigger: {
-    trigger: "#hand1",
-    start: "0 95%",
-    end: "50% 55%",
-    scrub: true,
-  },
-});
-
-
-gsap.from("#text4", {
-  y: 400,
+gsap.from("#brain-img", {
+  y: 600,
   scrollTrigger: {
     trigger: "#sec-4",
-    start: "0 95%",
-    end: "50% 55%",
+    start: "20% 60%",
+    end: "90% 90%",
     scrub: true,
+    toggleActions: "play none none reverse",
   },
 });
 
+gsap.from("#hand-img", {
+  y: 500,
+  scrollTrigger: {
+    trigger: "#sec-4",
+    start: "0 75%",
+    end: "90% 50%",
+    scrub: true,
+    toggleActions: "play none none reverse",
+  },
+});
 
-
-
-
+gsap.from("#sec-4-text", {
+  y: 600,
+  scrollTrigger: {
+    trigger: "#sec-4",
+    start: "0 75%",
+    end: "90% 50%",
+    scrub: true,
+    toggleActions: "play none none reverse",
+  },
+});
 
 // -------------Effect-5---------------//
 
-
-
-gsap.from("#rocket2", {
-  y: 400,
+gsap.from("#rocket", {
+  y: 800,
   scrollTrigger: {
-    trigger: "#rocket1",
-    start: "0 95%",
-    end: "50% 55%",
+    trigger: "#sec-5",
+    start: "0% 100%",
+    end: "70% 50%",
     scrub: true,
   },
 });
 
-
-gsap.from("#world2", {
+gsap.from("#world", {
   y: 400,
   scrollTrigger: {
     trigger: "#world1",
-    start: "0 95%",
-    end: "50% 85%",
+    start: "0% 35%",
+    end: "90% 95%",
     scrub: true,
   },
 });
 
-
-gsap.from("#text5", {
-  y: 400,
+gsap.from("#sec-5-text", {
+  y: 600,
   scrollTrigger: {
     trigger: "#sec-5",
-    start: "0 95%",
-    end: "50% 55%",
+    start: "0 75%",
+    end: "90% 50%",
     scrub: true,
   },
 });
 // -------------Effect-6---------------//
 
-
-
-gsap.from("#lights2", {
-  y: 400,
-  scrollTrigger: {
-    trigger: "#lights51",
-    start: "0 95%",
-    end: "50% 85%",
-    scrub: true,
-  },
-});
-
-
-gsap.from("#booot2", {
-  y: 400,
-  scrollTrigger: {
-    trigger: "#booot1",
-    start: "0 95%",
-    end: "50% 95%",
-    scrub: true,
-  },
-});
-
-
-gsap.from("#text6", {
-  y: 400,
+gsap.from("#lights-img", {
+  y: 900,
   scrollTrigger: {
     trigger: "#sec-6",
-    start: "0 95%",
-    end: "50% 55%",
+    start: "10% 60%",
+    end: "90% 90%",
+    scrub: true,
+  },
+});
+
+gsap.from("#booot-img", {
+  y: 300,
+  scrollTrigger: {
+    trigger: "#sec-6",
+    start: "50 50%",
+    end: "90% 85%",
+    scrub: true,
+  },
+});
+
+gsap.from("#sec-6-text", {
+  y: 600,
+  scrollTrigger: {
+    trigger: "#sec-6",
+    start: "0 75%",
+    end: "90% 50%",
     scrub: true,
   },
 });
