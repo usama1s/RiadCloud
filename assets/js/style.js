fd27e476ce6759c327de34e011e54d795859a23a
usamashaukt <usamashaukat36@gmail.com>
2024-09-10 00:57:13 +0500

    Added responisiveness of few sections

--- assets/js/style.js
+++ assets/js/style.js
@@ -656,7 +656,7 @@ $(document).ready(function () {
         {
           breakpoint: 576,
           settings: {
-            slidesToShow: 1,
+            slidesToShow: 2,
             slidesToScroll: 1,
           },
         },
